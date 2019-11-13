21f3a57b015901592db4460b9186a8b5a65dee6f
panManfredini <pan.manfredini@gmail.com>
2019-11-13 20:09:06 +0100

    first working version

---
+++ Examples/Kafka/kafkaAvro.js
@@ -0,0 +1,88 @@
+var Kafka = require('node-rdkafka');
+const registry = require('avro-schema-registry')('http://localhost:8081');
+const fs = require('fs');
+
+var producer = new Kafka.HighLevelProducer({
+  'metadata.broker.list': 'localhost:9092',
+});
+
+
+
+let RPC_schema = JSON.parse(fs.readFileSync("AvroSchemas/Request.json"));
+let message = {
+    method :"write",
+    // here the value must be stringyfied for maximal flexibility, this approach has the advantages:
+    // - we can use the same schema for all request (some producer have problems in sendig multiple schemas on same topic). One could go around it with union schemas but has a tedius sintax
+    // - On the OPC-proxy side the value will be converted to the proper OPC type
+    params : ["ciao", "78.9"],   
+    id : null 
+}
+  
+  producer.setValueSerializer(async function(v) {
+    try{
+        console.log("sending message ", v);
+        let msg = await registry.encodeMessage('OPC-test-10', RPC_schema, v);
+        //let msg = await schemaRegistry.encodeBySubject(v, 'subjectName-9')
+        console.log("sending message ", msg);
+        return msg;  
+    }
+    catch(e){
+        console.log(e);
+    }
+});
+
+producer.connect();
+producer.on("ready", ()=>{
+    producer.produce('OPC-test-10', null, 
+        message,
+       "hey", Date.now(), function(err, offset) {
+        // The offset if our acknowledgement level allows us to receive delivery offsets
+         //producer.disconnect();
+         console.log("the offset is :" + offset);
+         console.log(err);
+        producer.disconnect();
+
+    });
+    
+});
+
+
+
+
+/// ----- CONSUMER ------ ///
+/*
+var consumer = new Kafka.KafkaConsumer({
+    'group.id': 'NodeConsumer',
+    'metadata.broker.list': 'localhost:9092',
+    'offset_commit_cb': function(err, topicPartitions) {
+  
+      if (err) {
+        // There was an error committing
+        console.error(err);
+      } else {
+        // Commit went through. Let's log the topic partitions
+        console.log(topicPartitions);
+      }
+  
+    }
+  });
+
+consumer.connect();
+
+consumer
+  .on('ready', function() {
+    consumer.subscribe(['test-topic-x']);
+    consumer.consume();
+})
+  .on('data', async function(data) {
+    // Output the actual message contents
+    try{
+       let key = await registry.decode( data.key );
+       let message = await registry.decode( data.value );
+       console.log("topic: ",data.topic ,"  key :", key, "  value: ", message.value, " type: ", message.type, "  timestamp: ",  new Date(data.timestamp));
+    }
+    catch(e){
+        console.log("error: ", e);
+    }
+});
+*/
